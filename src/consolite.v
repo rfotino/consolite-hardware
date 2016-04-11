7468a6716808b336912ba2992a9f52880b53791c
Robert Fotino <robert.fotino@gmail.com>
2016-04-10 18:37:54 -0700

    Sets up 7-seg display and LPDDR RAM interface.
    The seven segment display has 3 digits and will be used for displaying
    status/error codes to the user for debugging. It currently tells if the
    LPDDR RAM is done calibrating or has an error. I have also included the
    UCF file which has the signals for LPDDR RAM, seven segment display,
    and system clock with many other modules there but still commented. In
    addition I have included a file called MimasV2Config.py, which is a script
    for flashing the Mimas V2 on Linux. The LPDDR RAM interface module has
    been generated by the Xilinx ISE software, but I have modified it slightly
    so that the c3_sys_clk signal is not buffered. This was causing problems
    when using the master clock both in the RAM and elsewhere because
    apparently an input can't drive both buffered and unbuffered components.
    Some minimal ISE project files are also included.

---
+++ src/consolite.v
@@ -0,0 +1,119 @@
+module consolite
+  (
+   // 100MHz system clock
+   input         clk,
+   // 7-segment display outputs
+   output [7:0]  seven_seg,
+   output [2:0]  seven_seg_en,
+   // LPDDR RAM inputs and outputs
+   inout [15:0]  mcb3_dram_dq,
+   output [12:0] mcb3_dram_a,
+   output [1:0]  mcb3_dram_ba,
+   output        mcb3_dram_cke,
+   output        mcb3_dram_ras_n,
+   output        mcb3_dram_cas_n,
+   output        mcb3_dram_we_n,
+   output        mcb3_dram_dm,
+   inout         mcb3_dram_udqs,
+   inout         mcb3_rzq,
+   output        mcb3_dram_udm,
+   input         c3_sys_rst_n,
+   inout         mcb3_dram_dqs,
+   output        mcb3_dram_ck,
+   output        mcb3_dram_ck_n,
+   output        mcb3_calib_done,
+   input         mcb3_error
+   );
+
+   // Main memory command signals
+   reg           lpram_cmd_en = 0;
+   reg [2:0]     lpram_cmd_instr = 0;
+   reg [5:0]     lpram_cmd_bl = 0; // bl = burst length
+   reg [29:0]    lpram_cmd_addr = 0;
+   wire          lpram_cmd_empty;
+   wire          lpram_cmd_full;
+
+   // Main memory write signals
+   reg           lpram_w_en = 0;
+   reg [3:0]     lpram_w_mask = 0;
+   reg [31:0]    lpram_w_data = 0;
+   wire          lpram_w_full;
+   wire          lpram_w_empty;
+   wire [6:0]    lpram_w_count;
+   wire          lpram_w_underrun;
+   wire          lpram_w_error;
+
+   // Main memory read signals
+   reg           lpram_r_en = 0;
+   wire [31:0]   lpram_r_data = 0;
+   wire          lpram_r_full;
+   wire          lpram_r_empty;
+   wire [6:0]    lpram_r_count;
+   wire          lpram_r_overflow;
+   wire          lpram_r_error;
+
+   // The 7-segment display for displaying state/error information
+   // to the user
+   seg_display seg_display_
+     (
+      .clk(clk),
+      .mcb3_calib_done(mcb3_calib_done),
+      .mcb3_error(mcb3_error),
+      .seven_seg(seven_seg),
+      .seven_seg_en(seven_seg_en)
+      );
+
+   // Create an instance of the LPDDR memory interface
+   s6_lpddr_ram main_ram_
+     (
+      // Command signals
+      .c3_p0_cmd_clk(clk),
+      .c3_p0_cmd_en(lpram_cmd_en),
+      .c3_p0_cmd_instr(lpram_cmd_instr),
+      .c3_p0_cmd_bl(lpram_cmd_bl),
+      .c3_p0_cmd_byte_addr(lpram_cmd_addr),
+      .c3_p0_cmd_empty(lpram_cmd_empty),
+      .c3_p0_cmd_full(lpram_cmd_full),
+
+      // Write signals
+      .c3_p0_wr_clk(clk),
+      .c3_p0_wr_en(lpram_w_en),
+      .c3_p0_wr_mask(lpram_w_mask),
+      .c3_p0_wr_data(lpram_w_data),
+      .c3_p0_wr_full(lpram_w_full),
+      .c3_p0_wr_empty(lpram_w_empty),
+      .c3_p0_wr_count(lpram_w_count),
+      .c3_p0_wr_underrun(lpram_w_underrun),
+      .c3_p0_wr_error(lpram_w_error),
+
+      // Read signals
+      .c3_p0_rd_clk(clk),
+      .c3_p0_rd_en(lpram_r_en),
+      .c3_p0_rd_data(lpram_r_data),
+      .c3_p0_rd_full(lpram_r_full),
+      .c3_p0_rd_empty(lpram_r_empty),
+      .c3_p0_rd_count(lpram_r_count),
+      .c3_p0_rd_overflow(lpram_r_overflow),
+      .c3_p0_rd_error(lpram_r_error),
+
+      // Memory interface signals
+      .mcb3_dram_dq(mcb3_dram_dq),
+      .mcb3_dram_a(mcb3_dram_a),
+      .mcb3_dram_ba(mcb3_dram_ba),
+      .mcb3_dram_cke(mcb3_dram_cke),
+      .mcb3_dram_ras_n(mcb3_dram_ras_n),
+      .mcb3_dram_cas_n(mcb3_dram_cas_n),
+      .mcb3_dram_we_n(mcb3_dram_we_n),
+      .mcb3_dram_dm(mcb3_dram_dm),
+      .mcb3_dram_udqs(mcb3_dram_udqs),
+      .mcb3_rzq(mcb3_rzq),
+      .mcb3_dram_udm(mcb3_dram_udm),
+      .c3_sys_clk(clk),
+      .c3_sys_rst_n(c3_sys_rst_n),
+      .c3_calib_done(mcb3_calib_done),
+      .mcb3_dram_dqs(mcb3_dram_dqs),
+      .mcb3_dram_ck(mcb3_dram_ck),
+      .mcb3_dram_ck_n(mcb3_dram_ck_n)
+      );
+
+endmodule
---
+++ src/seg_display.v
@@ -0,0 +1,85 @@
+`define STATE_MEM_UNCALIB  12'h500
+`define STATE_MEM_ERROR    12'h501
+`define STATE_DEFAULT      12'h100
+
+module seg_display
+  (
+   input wire       clk,
+   input wire       mcb3_calib_done, // memory interface calibration done
+   input wire       mcb3_error, // memory interface error
+   output reg [7:0] seven_seg,
+   output reg [2:0] seven_seg_en
+   );
+
+   // The 3 hex digits to display
+   reg [11:0]       state;
+
+   // Determine the current state based on input signals
+   always @ (*) begin
+      if (!mcb3_calib_done) begin
+	 state = `STATE_MEM_UNCALIB;
+      end else if (mcb3_error) begin
+	 state = `STATE_MEM_ERROR;
+      end else begin
+	 state = `STATE_DEFAULT;
+      end
+   end
+
+   // Divide the clock so that we get a seg_clk that runs at
+   // about 380Hz (if clk is 100MHz)
+   reg [17:0] counter = 0;
+   reg        seg_clk = 0;
+   always @ (posedge clk) begin
+      counter <= counter + 1;
+      seg_clk <= &counter;
+   end
+
+   // Which digit are we currently displaying?
+   reg [1:0] cur_digit = 0;
+
+   always @ (posedge seg_clk) begin
+      // Increment the digit
+      if (cur_digit == 2) begin
+	 cur_digit <= 0;
+      end else begin
+	 cur_digit <= cur_digit + 1;
+      end
+      // Display the current digit
+      if (cur_digit == 2) begin
+	 seven_seg_en <= 3'b011;
+	 display_digit(state[11:8]);
+      end else if (cur_digit == 1) begin
+	 seven_seg_en <= 3'b101;
+	 display_digit(state[7:4]);
+      end else begin
+	 seven_seg_en <= 3'b110;
+	 display_digit(state[3:0]);
+      end
+   end
+
+   // Display a 4-bit digit in hexadecimal
+   task display_digit;
+      input [3:0] d;
+      begin
+	 case (d)
+	   0: seven_seg <= 8'b00000011;
+	   1: seven_seg <= 8'b10011111;
+	   2: seven_seg <= 8'b00100101;
+	   3: seven_seg <= 8'b00001101;
+	   4: seven_seg <= 8'b10011001;
+	   5: seven_seg <= 8'b01001001;
+	   6: seven_seg <= 8'b01000001;
+	   7: seven_seg <= 8'b00011111;
+	   8: seven_seg <= 8'b00000001;
+	   9: seven_seg <= 8'b00001001;
+	   10: seven_seg <= 8'b00010001; // A
+	   11: seven_seg <= 8'b11000001; // b
+	   12: seven_seg <= 8'b01100011; // C
+	   13: seven_seg <= 8'b10000101; // d
+	   14: seven_seg <= 8'b01100001; // E
+	   15: seven_seg <= 8'b01110001; // F
+	 endcase
+      end
+   endtask
+
+endmodule
